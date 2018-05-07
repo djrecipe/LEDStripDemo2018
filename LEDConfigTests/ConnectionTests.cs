43c20f7cfeab43810968e6b2f76292c3f7c6d96c
jpierce <jd.pierce@promega.com>
2018-05-07 11:18:02 -0700

    added unit test project

---
+++ LEDConfigTests/ConnectionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LEDConfig.Tests
+{
+    [TestClass]
+    public class ConnectionTests
+    {
+        [TestMethod, TestCategory("Hardware")]
+        public void ConnectNano()
+        {
+        }
+    }
+}
---
+++ LEDConfigTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LEDConfigTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LEDConfigTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("39dc5348-b7ea-4f31-8431-34d0a9d0b191")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
