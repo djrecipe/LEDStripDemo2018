6c6a064013bf380bf452aa79c796ed3823a197af
jpierce <jd.pierce@promega.com>
2018-06-29 15:43:16 -0700

    added test class for logging

---
+++ LEDConfigTests/LogTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbaciLabs.LEDConfig;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LEDConfig.Tests
+{
+    [TestClass]
+    public class LogTests
+    {
+        [TestMethod]
+        public void TestLogInfo()
+        {
+            LogManager.Log.Info("TestLogInfo()");
+            return;
+        }
+    }
+}
